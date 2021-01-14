e443bf0ad8df3893f3c903566900659fc966f1e5
amandasaid <amandasaid@hks.harvard.edu>
2021-01-14 13:37:15 -0500

    complete step 2

--- example7/convert.py
+++ example7/convert.py
@@ -5,17 +5,30 @@ import datetime
 
 # 1. load input.csv into a variable called `polls`
 
+with open('input.csv') as f:
+    reader = csv.DictReader(f)
+    polls = list(reader)
+
 # 2. write a new file called output.csv and write a row with two headers: "date" and "approve"
 
+with open('output.csv', 'w') as f:
+	writer = csv.writer(f)
+	writer.writerow(['date', 'approve'])
+
 # 3. Loop through each row of `polls` 
+	for row in polls:
+
 
     # 4. and within that loop... convert the format of `enddate` from "1/22/2017" to "22-Jan-17"
     # hint: to read the date you will need to use
     #       date = datetime.datetime.strptime(myrawstring, "insert input format here")
-    #
+		date = datetime.datetime.strptime(row['enddate'], "%m/%d/%Y")
+		new_date = date.strftime("%-d-%b-%y")
+
     #       and to write the date you will need to use something like 
     #       new_date = date.strftime("insert output format here")
     # 
     #       date formats can be found at https://strftime.org/
     
-    # 5. write a new row of data with the transformed date and value for "approve" 
+    # 5. write a new row of data with the transformed date and value for "approve"
+		writer.writerow([new_date, row['approve']])
